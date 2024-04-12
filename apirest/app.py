f4fef1f51bec2275b17cdcd744bb9c1b9589f67a
Carlos Arboleda <c.arboledaz@uniandes.edu.co>
2024-04-12 00:36:32 -0500

    Some changes to get the basic project app and running Rename the "user" service to  "apirest" for more consistency

---
+++ apirest/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_restful import Api
+from .views import *
+from .models import db
+from os import environ
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = environ.get('DB_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
+
+app_context = app.app_context()
+app_context.push()
+
+db.init_app(app)
+db.create_all()
+
+api = Api(app)
+api.add_resource(TasksView, '/api/tasks') 
+api.add_resource(TaskView, '/api/tasks/<int:id_task>') 
+api.add_resource(SignInView, '/api/auth/signup')
+api.add_resource(LogInView, '/api/auth/login')
