9fad9d912e2736bb0e53d730cd599db8adb0fbdc
Hernan Alvarez Caballero <hd.alvarez@uniandes.edu.co>
2024-04-14 12:29:41 -0500

    Upload video

--- user/app.py
+++ user/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
-from .views import *
-from .models import db
+from views import *
+from models import db
 from os import environ
 from flask_jwt_extended import JWTManager
 
@@ -11,6 +11,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = environ.get('DB_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.config['JWT_SECRET_KEY'] = 'secret-phrase'
+app.config['MAX_CONTENT_LENGTH'] = 25 * 1024 * 1024
 
 app_context = app.app_context()
 app_context.push()
