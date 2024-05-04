90b4b8f0626fcc9b183635609237d4faa778c511
dparejaUniandes <d.pareja@uniandes.edu.co>
2024-05-04 12:06:40 -0500

    Changed the redis connection

--- worker/tasks.py
+++ worker/tasks.py
@@ -3,8 +3,9 @@ import cv2
 import requests
 from requests.exceptions import RequestException
 from os import environ
+from google.cloud import storage
 
-celery_app = Celery('tasks', broker=environ.get('CELERY_BROKER_URL'))
+celery_app = Celery('tasks', broker="redis://10.128.0.20:6379")
 
 @celery_app.task(bind=True, name='process_video')
 def process_video(self, video_path, filename, task_id):
