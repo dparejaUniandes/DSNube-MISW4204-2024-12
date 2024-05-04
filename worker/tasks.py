a82b94d8e7e3c2c718cef85b52c9ae562c526099
jose garcia <ing.manu@gmail.com>
2024-05-04 17:10:30 -0500

    inclusion worker

--- worker/tasks.py
+++ worker/tasks.py
@@ -3,6 +3,7 @@ import cv2
 import requests
 from requests.exceptions import RequestException
 from os import environ
+from google.cloud import storage
 
 celery_app = Celery('tasks', broker=environ.get('CELERY_BROKER_URL'))
 
@@ -11,8 +12,16 @@ def process_video(self, video_path, filename, task_id):
     print("******, ", video_path)
     logo_path = 'videos/logo.png'
 
+    bucket_name = 'fancy-store-folkloric-union-420902'
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+
     try:
-        video = cv2.VideoCapture(video_path)
+        blob = bucket.blob("videos/" + filename)
+        blob.download_to_filename('/tmp/' + filename)
+
+        video = cv2.VideoCapture('/tmp/' + filename)
         logo = cv2.imread(logo_path)
 
         fps = video.get(cv2.CAP_PROP_FPS)
@@ -27,7 +36,7 @@ def process_video(self, video_path, filename, task_id):
         logo = cv2.resize(logo, (new_width, new_height))
 
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        output_video = cv2.VideoWriter(f"/home/ing_manu/remote-videos/processed_{filename}", fourcc, fps, (new_width, new_height))
+        output_video = cv2.VideoWriter(f"/tmp/processed_{filename}", fourcc, fps, (new_width, new_height))
 
         max_duration = int(fps * 20)
 
@@ -44,8 +53,11 @@ def process_video(self, video_path, filename, task_id):
 
             frame_count += 1
         output_video.write(logo)
+        processed_blob_name = f"videos/processed_{filename}"
+        processed_blob = bucket.blob(processed_blob_name)
+        processed_blob.upload_from_filename(f"/tmp/processed_{filename}")
 
-        url = f"http://34.132.255.5:8080/api/tasks/{task_id}"
+        url = f"http://34.41.186.142:8080/api/tasks/{task_id}"
         data = {
             "name": f"processed_{filename}",
             "video_path": f"videos/processed_{filename}"
