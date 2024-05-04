d78de12469f91e33ffae50ac5d086a374cf5b8e1
jose garcia <ing.manu@gmail.com>
2024-05-04 09:42:54 -0500

    ruta bucket v1

--- user/views/views.py
+++ user/views/views.py
@@ -85,7 +85,7 @@ class TasksView(Resource):
 
         storage_client = storage.Client()
         bucket = storage_client.bucket(bucket_name)
-        blob = bucket.blob(pre_processed_filename)
+        blob = bucket.blob(video_path)
         blob.upload_from_file(video_file)
         video_url = blob.public_url
 
