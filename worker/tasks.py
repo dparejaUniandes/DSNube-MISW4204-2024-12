f47f3ba19766087dc17b8801a9cf7d52fe7eb41d
dparejaUniandes <d.pareja@uniandes.edu.co>
2024-05-08 07:43:42 -0500

    Created the publish and subscriber code or gcp

--- worker/tasks.py
+++ worker/tasks.py
@@ -1,15 +1,39 @@
 from tempfile import NamedTemporaryFile
-from celery import Celery
 import cv2
 import requests
 from requests.exceptions import RequestException
 from os import environ, remove
 from google.cloud import storage
+from concurrent.futures import TimeoutError
+from google.cloud import pubsub_v1
 
-celery_app = Celery('tasks', broker="redis://10.128.0.20:6379")
+# References:
+# Get JSON messages: https://cloud.google.com/pubsub/docs/samples/pubsub-subscriber-async-pull-custom-attributes?hl=es-419#pubsub_subscriber_async_pull_custom_attributes-python
 
-@celery_app.task(bind=True, name='process_video')
-def process_video(self, video_path, filename, task_id):
+
+project_id = "your-project-id"
+subscription_id = "your-subscription-id"
+timeout = 5.0
+
+subscriber = pubsub_v1.SubscriberClient()
+subscription_path = subscriber.subscription_path(project_id, subscription_id)
+
+def callback(message: pubsub_v1.subscriber.message.Message) -> None:
+    print(f"Received {message.data!r}.")
+    if message.attributes:
+        print("Attributes:")
+        for key in message.attributes:
+            if key == 'video_path':
+                video_path = message.attributes.get(key)
+            elif key == 'filename':
+                filename = message.attributes.get(key)
+            elif key == 'task_id':
+                task_id = message.attributes.get(key)
+            print(f"{key}: {video_path} {key}: {filename} {key}: {task_id}")
+    message.ack()
+    process_video(video_path, filename, task_id)
+
+def process_video(video_path, filename, task_id):
     print("*****, ", video_path)
     logo_path = 'videos/logo.png'
 
@@ -69,7 +93,6 @@ def process_video(self, video_path, filename, task_id):
             print(f"Tarea {task_id} actualizada exitosamente")
         except RequestException as e:
             print(f"Error al actualizar la tarea {task_id}: {str(e)}")
-            raise self.retry(exc=e, countdown=60)
 
     finally:
         video.release()
@@ -79,3 +102,16 @@ def process_video(self, video_path, filename, task_id):
         # Borrar los archivos temporales
         remove(temp_video_path)
         remove(temp_output.name)
+
+streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+print(f"Listening for messages on {subscription_path}..\n")
+
+# Wrap subscriber in a 'with' block to automatically call close() when done.
+with subscriber:
+    try:
+        # When `timeout` is not set, result() will block indefinitely,
+        # unless an exception is encountered first.
+        streaming_pull_future.result(timeout=timeout)
+    except TimeoutError:
+        streaming_pull_future.cancel()  # Trigger the shutdown.
+        streaming_pull_future.result()  # Block until the shutdown is complete.
